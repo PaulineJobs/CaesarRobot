eec86e435985e3a59555b85413515069d2df2221
willy.beller <willybeller@hotmail.fr>
2022-04-29 12:03:11 +0200

    update ihm

--- androidProject/app/src/main/java/com/upssitech/caesarihm/MainActivity.java
+++ androidProject/app/src/main/java/com/upssitech/caesarihm/MainActivity.java
@@ -3,6 +3,7 @@ package com.upssitech.caesarihm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
